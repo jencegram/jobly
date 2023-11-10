0466398092b377cbccbd203e1655ed8ece8c3f06
Jennifer <jencegram@users.noreply.github.com>
2023-11-09 22:54:22 -0500

    Add tests for Job model covering creation, updates, and errors

---
+++ models/jobs.test.js
@@ -0,0 +1,163 @@
+"use strict";
+
+const db = require("../db");
+const { BadRequestError, NotFoundError } = require("../expressError");
+const Job = require("./jobs"); 
+const {
+  commonBeforeAll,
+  commonBeforeEach,
+  commonAfterEach,
+  commonAfterAll,
+  testJobIds,
+} = require("./_testCommon");
+
+beforeAll(commonBeforeAll);
+beforeEach(commonBeforeEach);
+afterEach(commonAfterEach);
+afterAll(commonAfterAll);
+
+/************************************** create */
+
+describe("create", function () {
+  const newJob = {
+    title: "New",
+    salary: 100000,
+    equity: "0.1",
+    companyHandle: "c1",
+  };
+
+  test("works", async function () {
+    let job = await Job.create(newJob);
+    expect(job).toEqual({
+      id: expect.any(Number),
+      ...newJob,
+    });
+
+    const result = await db.query(
+      `SELECT id, title, salary, equity, company_handle AS "companyHandle"
+       FROM jobs
+       WHERE id = $1`, [job.id]);
+    expect(result.rows[0]).toEqual(job);
+  });
+
+  test("fails with duplicate job data", async function () {
+    try {
+      await Job.create(newJob);
+      await Job.create(newJob);
+      fail();
+    } catch (err) {
+      expect(err instanceof BadRequestError).toBeTruthy();
+    }
+  });  
+});
+
+/************************************** findAll */
+
+describe("findAll", function () {
+  test("works: returns all jobs", async function () {
+    let jobs = await Job.findAll();
+    expect(jobs).toEqual([
+      {
+        id: expect.any(Number),
+        title: "Job1",
+        salary: 100000,
+        equity: "0.1",
+        companyHandle: "c1",
+      },
+      {
+        id: expect.any(Number),
+        title: "Job2",
+        salary: 120000,
+        equity: "0.2",
+        companyHandle: "c1",
+      },
+      {
+        id: expect.any(Number),
+        title: "Job3",
+        salary: 110000,
+        equity: null,
+        companyHandle: "c2",
+      },
+    ]);
+  });
+});
+
+/************************************** get */
+
+describe("get", function () {
+  test("works", async function () {
+    let job = await Job.get(testJobIds[0]);
+    expect(job).toEqual({
+      id: testJobIds[0],
+      title: "Job1",
+      salary: 100000,
+      equity: "0.1",
+      companyHandle: "c1",
+    });
+  });
+
+  test("not found if no such job", async function () {
+    try {
+      await Job.get(0); 
+      fail();
+    } catch (err) {
+      expect(err instanceof NotFoundError).toBeTruthy();
+    }
+  });
+});
+
+/************************************** update */
+
+describe("update", function () {
+  const updateData = {
+    title: "Updated",
+    salary: 150000,
+    equity: "0.2",
+  };
+
+  test("works", async function () {
+    let job = await Job.update(testJobIds[0], updateData);
+    expect(job).toEqual({
+      id: testJobIds[0],
+      ...updateData,
+      companyHandle: "c1",
+    });
+
+    const result = await db.query(
+          `SELECT id, title, salary, equity, company_handle AS "companyHandle"
+           FROM jobs
+           WHERE id = $1`, [testJobIds[0]]);
+    expect(result.rows[0]).toEqual(job);
+  });
+
+  test("not found if no such job", async function () {
+    try {
+      await Job.update(0, updateData); 
+      fail();
+    } catch (err) {
+      expect(err instanceof NotFoundError).toBeTruthy();
+    }
+  });
+
+  // ... other tests ...
+});
+
+/************************************** remove */
+
+describe("remove", function () {
+  test("works", async function () {
+    await Job.remove(testJobIds[0]);
+    const res = await db.query(
+        "SELECT id FROM jobs WHERE id = $1", [testJobIds[0]]);
+    expect(res.rows.length).toEqual(0);
+  });
+
+  test("not found if no such job", async function () {
+    try {
+      await Job.remove(0); 
+      fail();
+    } catch (err) {
+      expect(err instanceof NotFoundError).toBeTruthy();
+    }
+  });
+});
