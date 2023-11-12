2a86e85232ad1f87d286d8b5491150d2d55438f0
Jennifer <jencegram@users.noreply.github.com>
2023-11-11 19:52:02 -0500

    Update company model to include job details in GET request

--- models/company.js
+++ models/company.js
@@ -88,30 +88,39 @@ class Company {
   }
 
   /** Given a company handle, return data about company.
-   *
-   * Returns { handle, name, description, numEmployees, logoUrl, jobs }
-   *   where jobs is [{ id, title, salary, equity, companyHandle }, ...]
-   *
-   * Throws NotFoundError if not found.
-   */
-  static async get(handle) {
-    const companyRes = await db.query(
-      `SELECT handle,
-              name,
-              description,
-              num_employees AS "numEmployees",
-              logo_url AS "logoUrl"
-       FROM companies
-       WHERE handle = $1`,
-      [handle]
-    );
+ *
+ * Returns { handle, name, description, numEmployees, logoUrl, jobs }
+ *   where jobs is [{ id, title, salary, equity }, ...]
+ *
+ * Throws NotFoundError if not found.
+ */
+static async get(handle) {
+  const companyRes = await db.query(
+    `SELECT handle,
+            name,
+            description,
+            num_employees AS "numEmployees",
+            logo_url AS "logoUrl"
+     FROM companies
+     WHERE handle = $1`,
+    [handle]
+  );
 
-    const company = companyRes.rows[0];
+  const company = companyRes.rows[0];
 
-    if (!company) throw new NotFoundError(`No company: ${handle}`);
+  if (!company) throw new NotFoundError(`No company: ${handle}`);
 
-    return company;
-  }
+  const jobsRes = await db.query(
+    `SELECT id, title, salary, equity
+     FROM jobs
+     WHERE company_handle = $1`,
+    [handle]
+  );
+
+  company.jobs = jobsRes.rows;
+
+  return company;
+}
 
   /** Update company data with `data`.
    *
